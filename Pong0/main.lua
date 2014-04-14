c11e27375dcd6d4a7b5a85e4d998716ca8bf4d6d
Bruno Guedes <professional.mole@yahoo.com.br>
2014-04-14 00:42:20 -0300

    Pong Gameplay Demo: initial commit

---
+++ Pong0/main.lua
@@ -0,0 +1,24 @@
+require("paddle")
+requre("ball")
+
+sound {}
+
+Paddles = {}
+Ball = {}
+
+function love.load()
+end
+
+function love.keypressed(key, isrepeat)
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+	Paddles:draw()
+	Ball:draw()
+end
+
+function love.quit()
+end
